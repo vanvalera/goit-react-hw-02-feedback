2be7e3df914ab47454e4e7c4df632bc1128036fe
vanvalera <vmarton777@gmail.com>
2023-05-31 15:09:29 +0300

    111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,6 +25,7 @@ export default class App extends Component {
         style={{
           height: '100vh',
           display: 'flex',
+          flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
