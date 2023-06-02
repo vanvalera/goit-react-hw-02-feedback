3ac43ab0d160a4c6431949772da1859fb6105535
vanvalera <vmarton777@gmail.com>
2023-06-02 23:07:31 +0300

    complete

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
@@ -16,4 +17,8 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
       ))}
     </div>
   );
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import classNames from 'classnames';
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 import { Notification } from 'components/Notification/Notification';
 export const Statistics = ({
   good,
@@ -12,14 +15,20 @@ export const Statistics = ({
   }
   return (
     <>
-      <p>Statistics</p>
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
+      <div className={classNames(css.font20)}>
+        <p className={css.good}>Good: {good}</p>
+        <p className={css.neutral}>Neutral: {neutral}</p>
+        <p className={css.bad}>Bad: {bad}</p>
         <p>Total: {total}</p>
         <p>Positive feedback: {positivePercentage}%</p>
       </div>
     </>
   );
+};
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
